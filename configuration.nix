36fb6f6309bf3dff76f1642efe6a735fc2c04052
vibewill <vibewill@gmail.com>
2024-06-29 09:27:45 -0300

    rails

---
+++ configuration.nix
@@ -0,0 +1,180 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  programs.zsh.enable = true;
+  virtualisation.docker.enable = true;
+  users.extraGroups.docker.members = [ "vibewill-with-access-to-socket" ];
+  ####nvidia#####
+  services.xserver.windowManager.qtile.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;  
+
+
+  # Adicione outros módulos do Qt conforme necessário
+  # Adicione o suporte ao KVM
+  
+  
+
+  virtualisation.virtualbox.host.enable = true; # se desejar suporte ao VirtualBox
+   
+
+
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  #####################################
+  # programs.hyprland.enable = true;
+  
+
+  # Enable networking
+  networking.networkmanager.enable = false;
+  security.rtkit.enable = true;
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+};
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+};
+  
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "pt_BR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "br";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vibewill = {
+    isNormalUser = true;
+    description = "vibewill";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  qtile
+  vscode
+  kitty
+  wofi
+  swaybg
+  firefox
+  python311Packages.pip
+  gcc
+  obs-studio
+  gimp
+  wine
+  wine64
+  steam
+  docker
+  google-chrome
+  woeusb
+  tor-browser
+  ruby
+  python3
+  go
+  lua
+  php
+  zola
+  hugo
+  lutris
+  woeusb
+  gimp
+  krita
+  cargo
+  xdg-user-dirs
+  metasploit
+  audacity
+  nmap
+  maltego
+  polkit_gnome
+  bundler
+  nodejs
+  wget
+  vim
+  python310Packages.tkinter
+  spotify
+  cava
+  jekyll
+  openshot-qt
+  python39Packages.tkinter
+  social-engineer-toolkit
+  ventoy
+  rubyPackages.rails           
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
